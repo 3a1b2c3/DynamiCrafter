a3423158f4c4d3cf18d83e4a3f8f99c1993c95a5
kschmid <kschmid@nvidia.com>
2025-05-29 17:12:43 +1000

    scale

--- evaluation/replicate_more_parameter/predict.py
+++ evaluation/replicate_more_parameter/predict.py
@@ -195,7 +195,6 @@ python evaluate.py --config configs/ldm/ldmvfi-vqflow-f32-c256-concat_max.yaml -
 
 image1_path = r"C:\workspace\cs231n\proj\data\kitti\TEST\image_03_straba\data_strassenbahn\%s_result.jpg"
 recursive_predict(image1_path, "a train on te side off the road", 1, 115, step=6)
-
 assert 1 == 2
 # for i in range(1, 99, 6):
 image1_path = (
@@ -203,7 +202,7 @@ image1_path = (
 )
 # for i in range(1, 238, 6):
 image1_path = r"C:\workspace\cs231n\proj\data\kitti\TEST\2011_09_26_drive_0106_sync\2011_09_26_rad\2011_09_26_drive_0106_sync_red_flying_cam_pedestrinas\image_03\data\%s_result.jpg"
-
+ prompt=str(i) + "_ slowly going down the street in Stuttgart",
 
 for i in range(1, 99, 6):
     n = str(i)
