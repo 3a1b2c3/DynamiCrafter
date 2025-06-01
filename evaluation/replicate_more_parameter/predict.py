353343aa5534766b20c7b470db6f8ef3f63d28ae
kschmid <kschmid@nvidia.com>
2025-06-01 16:30:54 +1000

    data

--- evaluation/replicate_more_parameter/predict.py
+++ evaluation/replicate_more_parameter/predict.py
@@ -9,6 +9,8 @@ ckpt = r"C:\workspace\cs231n\proj\DynamiCrafterLora\checkpoints\dynamicrafter_51
 ckpt = r"C:\workspace\cs231n\proj\DynamiCrafterLora\checkpoints\dynamicrafter_512_interp_v1\model_train_epoch=0-step=3-v2_applied.ckpt"
 ckpt = r"C:\workspace\cs231n\proj\DynamiCrafterLora\checkpoints\dynamicrafter_512_interp_v1\model_train.ckpt"
 ckpt = r"C:\workspace\cs231n\proj\DynamiCrafterLora\checkpoints\dynamicrafter_512_interp_v1\model_train_epoch=0-step=6_applied.ckpt"
+ckpt = r"C:\workspace\cs231n\proj\DynamiCrafterLora\checkpoints\dynamicrafter_512_interp_v1\model_train_epoch=2-step=39_applied.ckpt"
+#ckpt = r"C:\workspace\cs231n\proj\DynamiCrafterLora\checkpoints\dynamicrafter_512_interp_v1\model_train.ckpt"
 # C:\workspace\cs231n\proj\DynamiCrafter\configs\training_512_v1.0\config_interp.yaml
 # python apply_lora.py --format ckpt --base_model="C:\workspace\cs231n\proj\DynamiCrafterLora\checkpoints\dynamicrafter_512_interp_v1\model_train.ckpt" --lora=C:\workspace\cs231n\proj\DynamiCrafterLora\main\logs\test\checkpoints\old\epoch=0-step=9.ckpt --alpha=1.0
 directory = r"C:\workspace\cs231n\proj\DynamiCrafterLora\output"
@@ -154,13 +156,13 @@ def recursive_predict(
     eta=0.5,  # deterministic   eta=1.0
     fs=10,
     seed=122,
-    prefix="lora_",
+    prefix="lora",
 ):
     try:
         for i in range(start, end, img_step):
             n = str(i)
             image1_path = path1 % n.zfill(3)
-            n = str(i + 6)
+            n = str(i + img_step)
             image2_path = path1 % n.zfill(3)
             # assert os.path.exists(image1_path)
             image1 = Image.open(image1_path)
@@ -170,7 +172,7 @@ def recursive_predict(
             res6 = p.predict(
                 image1_path,
                 image2_path,
-                str(img_step) + ("_st_") + str(i) + prefix + (" .") + prompt,
+                str(img_step) + ("_st_") + (str(i).zfill(3)) + prefix + (" .") + prompt,
                 steps=80,  # steps=50,
                 cfg_scale=12,  # >10	Strongly follows the conditioning – may become overly sharp or brittle  cfg_scale=7.5,
                 eta=0.8,  # deterministic   eta=1.0
@@ -203,6 +205,39 @@ configs/inference_512_v1.0.yaml config_file: {'model': {'target': 'lvdm.models.d
 python evaluate.py --config configs/ldm/ldmvfi-vqflow-f32-c256-concat_max.yaml --ckpt <path/to/ldmvfi-vqflow-f32-c256-concat_max.ckpt> --dataset Middlebury_others --metrics PSNR SSIM LPIPS \
 --data_dir <path/to/data/dir> --out_dir eval_results/ldmvfi-vqflow-f32-c256-concat_max/  --use_ddim
 """
+#image1_path = r"C:\workspace\cs231n\proj\data\titan_data\TEST\clip_772\images\%s_result.jpg"
+#recursive_predict(image1_path, "shoppers in tokyon", 1, 108, 6)
+
+
+image1_path = r"C:\workspace\cs231n\proj\data\kitti\TEST\2011_09_26_drive_0001_sync\image_03\data\%s_result.jpg"
+recursive_predict(image1_path, "a train rides on the side off the road", 1, 108, 6)
+
+
+image1_path = r"C:\workspace\cs231n\proj\data\kitti\TEST\2011_09_26_drive_0014_sync_follow\2011_09_26\2011_09_26_drive_0014_sync\image_03\data\%s_result.jpg"
+prompt = "Going down a suburbian leavy empty road"
+recursive_predict(image1_path, prompt, 300, 500, 6)
+
+
+image1_path = r"C:\workspace\cs231n\proj\data\malaga-urban-dataset-extract-04\TEST\malaga-urban-dataset-extract-06\malaga-urban-dataset-extract-06\l\%s_result.jpg"
+#recursive_predict(image1_path, "driving around empty Malaga in slow motion", 1, 108, 3)
+
+image1_path = r"C:\workspace\cs231n\proj\data\malaga-urban-dataset-extract-04\TEST\malaga-urban-dataset-extract-06\malaga-urban-dataset-extract-06\l\%s_result.jpg"
+recursive_predict(image1_path, "driving around empty Malaga", 1, 108, 12)
+
+
+# for i in range(1, 99, 6):
+image1_path = (
+    r"C:\workspace\cs231n\proj\data\titan_data\TEST\clip_537\images\%s_result.jpg"
+)
+prompt = "discovering Tokyo backlanes"
+recursive_predict(image1_path, prompt, 1, 99, 6)
+
+
+# for i in range(1, 238, 6):
+image1_path = r"C:\workspace\cs231n\proj\data\kitti\TEST\2011_09_26_drive_0106_sync\2011_09_26_rad\2011_09_26_drive_0106_sync_red_flying_cam_pedestrinas\image_03\data\%s_result.jpg"
+prompt = "slowly going down the street in Stuttgart"
+recursive_predict(image1_path, prompt, 1, 238, 6)
+
 image1_path = (
     r"C:\workspace\cs231n\proj\data\titan_data\TEST\clip_486\images\020_result.jpg"
 )
@@ -219,10 +254,6 @@ recursive_predict(image1_path, prompt, 1, 100, 3)
 image1_path = r"C:\workspace\cs231n\proj\data\kitti\TEST\2011_09_26_drive_0001_sync\image_03\data\%s_result.jpg"
 recursive_predict(image1_path, "a train rides on the side off the road", 1, 108, 6)
 
-# for i in range(1, 238, 6):
-image1_path = r"C:\workspace\cs231n\proj\data\kitti\TEST\2011_09_26_drive_0106_sync\2011_09_26_rad\2011_09_26_drive_0106_sync_red_flying_cam_pedestrinas\image_03\data\%s_result.jpg"
-prompt = "slowly going down the street in Stuttgart"
-recursive_predict(image1_path, prompt, 1, 238, 3)
 
 # for i in range(1, 99, 6):
 image1_path = (
@@ -233,17 +264,7 @@ recursive_predict(image1_path, prompt, 1, 99, 3)
 
 
 
-# for i in range(1, 99, 6):
-image1_path = (
-    r"C:\workspace\cs231n\proj\data\titan_data\TEST\clip_537\images\%s_result.jpg"
-)
-prompt = "discovering Tokyo backlanes"
-recursive_predict(image1_path, prompt, 1, 99, 3)
 
-
-image1_path = r"C:\workspace\cs231n\proj\data\kitti\TEST\2011_09_26_drive_0014_sync_follow\2011_09_26\2011_09_26_drive_0014_sync\image_03\data\%s_result.jpg"
-prompt = "Going down a subrbian leavy empty road"
-recursive_predict(image1_path, prompt, 300, 500, 3)
 
 
 assert 1 == 2
